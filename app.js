c31dbfb499d0b89bd8101b450d293ae638ab9044
Jim Liu <jim.liu@tinklabs.com>
2019-05-09 18:11:41 +0800

    test20

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const {test} = require('./test');
 
 test();
+
+process
+  .on('unhandledRejection', (reason, p) => {
+    console.error(reason, 'Unhandled Rejection at Promise', p);
+    process.exit(1);
+  })
+  .on('uncaughtException', err => {
+    console.error(err, 'Uncaught Exception thrown');
+    process.exit(1);
+  });
