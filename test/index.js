c8aed4d8cb043938ea27c2c494233d58270cc2a4
Jim Liu <jim.liu@tinklabs.com>
2019-05-09 16:48:40 +0800

    test16

--- test/index.js
+++ test/index.js
@@ -2,32 +2,32 @@ const {
     downloadCollections
 } = require('../service');
 const exec = require('child_process').execSync;
-const runTest = ({
-    collectionList,
-    env,
-    pipelineName
-}) => {
+
+const runTest = ({collectionList, env, pipelineName}) => {
     for (let collectionName of collectionList) {
     	try{
     		let newmanCommand = `newman run files/collections/${collectionName}.json -e files/environments/${env}.json -r html,cli --reporter-html-export report-${collectionName}.html`;
 	        exec(newmanCommand, {
 	            cwd: process.cwd(),
-	            stdio: 'inherit'
+	            stdio: 'inherit',
+	            env: process.env
 	        });
     	}catch(e) {
-    		console.log('-------run newman errror', e);
+    		throw e;
     	}finally {
     		saveToS3(collectionName, pipelineName);
     	}
         
     }
 };
+
 const saveToS3 = (collectionName, pipelineName) => {
     try {
         const s3Command = `aws s3 cp ./report-${collectionName}.html s3://postman-reports/reports/${pipelineName}/${process.env.CURRENT_DATE}/report-${collectionName}.html`;
         exec(s3Command, {
             cwd: process.cwd(),
-            stdio: 'inherit'
+            stdio: 'inherit',
+            env: process.env
         });
     } catch (e) {
         console.log(e);
